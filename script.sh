9d527fdabbe4b610e97b3e650750113f26bb1573
Fatal Tempo <109779232+FatalTempo@users.noreply.github.com>
2024-11-21 23:58:03 -0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,37 +2,38 @@
 
 # if there is an \r error, go to notepad++, paste the script, ctrl+f and remove all \r
 # use readarray for when user copies and pastes
-allowedUsers=(root daemon bin sys sync games man lp mail news uucp proxy www-data backup list irc gnats nobody _apt ftp https)
+e () { echo; echo $1; }
+listIt () { # avoids being DRY
+  for user in $@; do
+    echo '  '$user
+  done
+}
+allowedUsers=(root daemon bin sys sync games man lp mail news uucp proxy www-data backup list irc gnats nobody _apt ftp https avahi saned)
 nonAuthUsers=()
 authedUsers=()
 
+echo "detected 'unsafe' user list:"
 for user in `cut -d: -f1 /etc/passwd`; do
   if [[ ! ${allowedUsers[@]} =~ $user ]]; then
-    echo "'$user'" is NOT authorized
+    echo '  '$user
     nonAuthUsers+=($user)
   else
     authedUsers+=($user)
   fi
 done
-echo
+
+e 'check for accuracy of course.'
+
 if (( ${#nonAuthUsers[@]} > 0 )); then # has sus user
   while :; do # inf loop til user_input = 1 or 5
-    listIt () {
-      for user in $@; do
-        echo '  '$user
-      done
-    }
-    e () { echo; echo $1; }
     PS3="> "
     select opt in "delete all non authorized users (runs the 'userdel -r user' cmd)" "list the SAFE users from the scan" "list the UNSAFE users from the scan" "list KNOWN safe users" "goto to main menu"; do
       case $REPLY in
         1)
-          echo
+          e
           for user in ${nonAuthUsers[@]}; do
-            echo deleting $user ...
-            userdel -r $user || echo failed to delete
+            userdel -r $user
             wait
-            echo deleted $user
           done
           break ;;
         2)
